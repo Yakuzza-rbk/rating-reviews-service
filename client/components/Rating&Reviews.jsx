a9dcdc9a9e4aa386a8bf8a6fe3c50a413c052ece
Nassir01 <you@example.com>
2021-05-28 15:09:10 +0200

    init

--- client/components/Rating&Reviews.jsx
+++ client/components/Rating&Reviews.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-export default class NameOfTheService extends React.Component {
+export default class RatingReviews extends React.Component {
   constructor() {
     super();
 
